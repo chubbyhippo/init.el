5e9814cdf0f0758373d013e7cc15feb16c665ff4
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-06-02 20:33:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@
 mkdir -p "$HOME"/.config/emacs/
 
 curl https://raw.githubusercontent.com/chubbyhippo/init.el/refs/heads/main/early-init.el -o "$HOME"/.config/emacs/early-init.el
+curl https://raw.githubusercontent.com/chubbyhippo/init.el/refs/heads/main/init.el -o "$HOME"/.config/emacs/init.el
