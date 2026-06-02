c1a569f6d656a7c8df23cca9a2789c3772664b82
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-06-02 20:13:38 +0700

    Update init.el

--- init.el
+++ init.el
@@ -2,3 +2,8 @@
 (pixel-scroll-precision-mode 1)
 (which-key-mode 1)
 (windmove-default-keybindings 'shift)
+
+(setopt auto-revert-avoid-polling t)
+(setopt auto-revert-interval 5)
+(setopt auto-revert-check-vc-info t)
+(global-auto-revert-mode 1)
