5454249b0e15c9ce2cee39567d60ac0aad1269db
chubbyhippo <mkmsacc@outlook.com>
2026-06-07 18:16:04 +0700

    Add `embark` and `embark-consult` packages with keybindings in `init.el`

--- init.el
+++ init.el
@@ -60,6 +60,23 @@
   :init
   (marginalia-mode 1))
 
+(use-package embark
+  :ensure t
+  :bind
+  (("C-." . embark-act)
+   ("C-;" . embark-dwim)
+   ("C-h B" . embark-bindings))
+  :init
+  (setq prefix-help-command #'embark-prefix-help-command)
+  :config
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+(use-package embark-consult
+  :ensure t)
+
 (use-package corfu
   :ensure t
   :custom
