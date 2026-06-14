2bba173ca3ac8878d8773a083766f33f589df803
chubbyhippo <mkmsacc@outlook.com>
2026-06-14 12:47:52 +0700

    Refine keybindings for `multiple-cursors` in `init.el`

--- init.el
+++ init.el
@@ -169,13 +169,12 @@
 (use-package multiple-cursors
   :ensure t
   :bind
-  (("C-c m l" . mc/edit-lines)
-   ("C-c m n" . mc/mark-next-like-this)
-   ("C-c m p" . mc/mark-previous-like-this)
-   ("C-c m a" . mc/mark-all-like-this)
-   ("C-c m d" . mc/mark-all-dwim)
-   ("C-c m N" . mc/skip-to-next-like-this)
-   ("C-c m P" . mc/skip-to-previous-like-this)))
+  (("C-S-c C-S-c" . mc/edit-lines)
+   ("C->" . mc/mark-next-like-this)
+   ("C-<" . mc/mark-previous-like-this)
+   ("C-M->" . mc/skip-to-next-like-this)
+   ("C-M-<" . mc/skip-to-previous-like-this)
+   ("C-c C-<" . mc/mark-all-like-this)))
 
 (use-package corfu-terminal ;; corfu popups in emacs -nw (Emacs 30 has no tty child frames)
   :ensure t
