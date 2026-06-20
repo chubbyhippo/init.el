c5f78db18542d1b58b63eb6cb9e21c991e4d4efa
chubbyhippo <mkmsacc@outlook.com>
2026-06-20 21:36:16 +0700

    Refine installation script: use `--batch` for non-interactive execution and wrap `install.el` logic in `progn`.

--- install.el
+++ install.el
@@ -1,9 +1,9 @@
-(require 'url)
-
-(let* ((base "https://raw.githubusercontent.com/chubbyhippo/init.el/refs/heads/main/")
-       (dir  (expand-file-name "~/.config/emacs/")))
-  (make-directory dir t)
-  (dolist (file '("early-init.el" "init.el"))
-    (let ((inhibit-message t))
-      (url-copy-file (concat base file) (expand-file-name file dir) t))
-    (princ (format "Installed %s -> %s\n" file dir))))
+(progn
+  (require 'url)
+  (let* ((base "https://raw.githubusercontent.com/chubbyhippo/init.el/refs/heads/main/")
+         (dir  (expand-file-name "~/.config/emacs/")))
+    (make-directory dir t)
+    (dolist (file '("early-init.el" "init.el"))
+      (let ((inhibit-message t))
+        (url-copy-file (concat base file) (expand-file-name file dir) t))
+      (princ (format "Installed %s -> %s\n" file dir)))))
