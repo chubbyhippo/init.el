ee1f8bb85b9139eef935f6a0334e78b02e1c8de1
chubbyhippo <mkmsacc@outlook.com>
2026-06-20 21:25:42 +0700

    Suppress URL status messages and replace `message` with `princ` for cleaner output in `install.el`.

--- install.el
+++ install.el
@@ -1,9 +1,9 @@
 (require 'url)
-(setq url-show-status nil)
 
 (let* ((base "https://raw.githubusercontent.com/chubbyhippo/init.el/refs/heads/main/")
        (dir  (expand-file-name "~/.config/emacs/")))
   (make-directory dir t)
   (dolist (file '("early-init.el" "init.el"))
-    (url-copy-file (concat base file) (expand-file-name file dir) t)
-    (message "Installed %s -> %s" file dir)))
+    (let ((inhibit-message t))
+      (url-copy-file (concat base file) (expand-file-name file dir) t))
+    (princ (format "Installed %s -> %s\n" file dir))))
