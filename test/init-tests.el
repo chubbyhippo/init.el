1a8607b743bd198935d9ba7d80087829b57ed0ec
chubbyhippo <mkmsacc@outlook.com>
2026-07-24 09:10:16 +0700

    Bring license back

--- test/init-tests.el
+++ test/init-tests.el
@@ -1,5 +1,22 @@
 ;;; init-tests.el --- BDD-style ERT suite for init.el  -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2026 Chubby Hippo
+;;
+;; This program is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+;; more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with this program. If not, see <https://www.gnu.org/licenses/>.
+;;
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
 ;; Headless run:
 ;;   emacs -Q --batch -l test/init-tests.el -f ert-run-tests-batch-and-exit
 ;; or ./test/run.sh (which points package-user-dir at the installed packages).
